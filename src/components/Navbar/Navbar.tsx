621c95549c919c16244612b80af1b031d7e43fad
ko-tenka <kotelnikova123.123@gmail.com>
2024-09-25 02:28:05 +0300

    reelase prob

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+import  { useState } from 'react';
+import './navbarglav.css';
+
+export default function Navbarglav() {
+
+  const [isImageClicked, setIsImageClicked] = useState(false);
+
+  const handleImageClick = () => {
+    setIsImageClicked(true);
+  };
+
+  const handCloseClicked = () => {
+    setIsImageClicked(false);
+  }
+
+  return (
+    <div className='navConteiner'>
+      <div className='spisok'>
+        <a className='comp1' style={{ color: 'white' }} href='#teachers'>педагоги</a>
+        <a className='comp1' style={{ color: 'white' }} href='#news'>новости</a>
+        <a className='comp1' style={{ color: 'white' }} href='#contacts'>контакты</a>
+      </div>
+      <div className='navConteiner_phone'>
+        {!isImageClicked ?(
+          <img 
+            className='menu' 
+            src="./menu.png" 
+            alt="меню" 
+            onClick={handleImageClick}  
+          />
+        ):(
+          <div className='spisok_phone'>
+            <img className="close" 
+              src="./close.png"
+              alt="крестик" 
+              onClick={handCloseClicked} />
+            <a className='comp2' onClick={handCloseClicked} href='#teachers'>педагоги</a>
+            <a className='comp2' onClick={handCloseClicked} href='#news'>новости</a>
+            <a className='comp2' onClick={handCloseClicked} href='#contacts'>контакты</a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Teacher/Teacher.tsx
@@ -0,0 +1,21 @@
+
+import './Teacher.css'
+
+export default function Teacher() {
+  return (
+    <div className='glav_conteiner'>
+        <div className='oneTeacher'>
+      <img className='photo-marina' src="./Marina.JPG" alt="Фото руководителя" />
+      <div className='opisanie'>
+      <p className='zagolovok'>Руководитель клуба</p>
+      <p className='text_marina'> Закончила Кузбасский колледж культуры и искусств имени народного артиста СССР Иосифа Давыдовича Кобзона! <br/>
+ Победительница всероссийский и международных конкурсов и фестивалей!   <br/>
+Хореограф, педагог и основатель школы современного танца « Maze dance company» по направлению contemporary , modern, детский танец!  <br/>
+Ученики школы являются победителями чемпионата «Scala Champ-Fest» в 2023 году!  <br/>
+Обладатели многократных побед на всероссийских и международных конкурсах и фестивалей!</p>
+</div>
+    </div>
+    {/* <img className='line' src="./line3.PNG" alt="линия" /> */}
+    </div>
+  )
+}
